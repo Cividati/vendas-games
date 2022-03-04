ade2df9f06904508d5c181970431047c7080c76c
Cividati <rubens.cividati.teles@gmail.com>
2022-03-04 16:12:35 -0300

    feat: added readAllDataAtOnce method

--- src/main/java/csvReader.java
+++ src/main/java/csvReader.java
@@ -1,6 +1,8 @@
 import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
 
 import java.io.FileReader;
+import java.util.List;
 
 public class csvReader {
     // Java code to illustrate reading a
@@ -22,6 +24,33 @@ public class csvReader {
             // we are going to read data line by line
             while ((nextRecord = csvReader.readNext()) != null) {
                 for (String cell : nextRecord) {
+                    System.out.print(cell + "\t");
+                }
+                System.out.println();
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    // Java code to illustrate reading a
+    // all data at once
+    public static void readAllDataAtOnce(String file)
+    {
+        try {
+            // Create an object of file reader
+            // class with CSV file as a parameter.
+            FileReader filereader = new FileReader(file);
+
+            // create csvReader object and skip first Line
+            CSVReader csvReader = new CSVReaderBuilder(filereader)
+                    .withSkipLines(1)
+                    .build();
+            List<String[]> allData = csvReader.readAll();
+
+            // print Data
+            for (String[] row : allData) {
+                for (String cell : row) {
                     System.out.print(cell + "\t");
                 }
                 System.out.println();
