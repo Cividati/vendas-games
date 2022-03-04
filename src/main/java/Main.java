5b903f63b700327a2640d9dfeaef470f83a95b91
Cividati <rubens.cividati.teles@gmail.com>
2022-03-04 16:55:44 -0300

    feat: Question 01 done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 import jdk.swing.interop.SwingInterOpUtils;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
@@ -9,6 +10,14 @@ private static final String CSV_PATH = "data/vendas-games.csv";
     public static void main(String[] args) throws IOException {
         csvReader csv = new csvReader();
 
-        csvReader.readCSV(CSV_PATH);
+        ArrayList<Jogo> arrayJogos =  csvReader.readCSV(CSV_PATH);
+
+        // 1. Filtrar e criar um ArrayList (ou coleção equivalente) buscando jogos de plataformas especificas. Exemplo: Filtrar no CSV jogos para PS4, ou para PC.
+        String platform = "PS4";
+        arrayJogos.forEach(jogo -> {
+            if (jogo.getPlatform().equals(platform)){
+                jogo.show();
+            }
+        });
     }
 }
